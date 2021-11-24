8d18125933e74d6ca04e6ed7259084cacf1e599f
AnnaGoko <94928523+AnnaGoko@users.noreply.github.com>
2021-11-24 17:44:11 +0000

    Задание 1.2

--- main.cpp
+++ main.cpp
@@ -3,23 +3,45 @@
 
 using namespace std;
 
-double CalculateA(double x, double y, double z) {
-    double a = (x * x * (x + 1)) / (y - pow(sin(x + z), 2));
-    return a;
+double FindThirdSide(double a, double b, double y) {
+    const double pi = 3.14;
+    double side = a*a + b*b - (2*a*b*cos((y*pi)/180));
+    side = sqrt(side);
+    return side;
 }
 
-double CalculateB(double x, double y, double z) {
-    double b = (sqrt((x * y) / z)) + pow(cos(pow(x + y, 2)), 2);
-    return b;
+double FindSqure(double a, double b, double y) {
+    const double pi = 3.14;
+    double s = ((a / 2)*b*sin((y*pi)/180)) / 2;
+    return s;
+}
+
+double RadiusOutside(double a, double b, double c) {
+    double p = (a + b + c) / 2;
+    double r = (a * b * c) / (4 * sqrt(p * (p - a) * (p - b) * (p - c)));
+    return r;
 }
 
 int main()
 {
     setlocale(0, "");
-    const double x = 0.5;
-    const double y = 0.05;
-    const double z = 0.7;
-    cout << "X = " << x << " Y = " << y << " Z = " << z << endl;
-    printf("Otvet:");
-    cout << "a = " << CalculateA(x, y, z) << "b = " << CalculateB(x, y, z) << endl;
+    double a;
+    double b;
+    double y;
+    cout << "Введите первую сторону: " << endl;
+    cin >> a;
+    cout << "Введите вторую сторону: " << endl;
+    cin >> b;
+    cout << "Введите угол между ними: " << endl;
+    cin >> y;
+    if (!cin)
+    {
+        cout << "Input Error. Enter THE NUMBER." << endl;
+        cin.clear();
+        fflush(stdin);
+    }
+    else {
+        double c = FindThirdSide(a, b, y);
+        cout << "Третья сторона = " << c << " Площадь = " << FindSqure(a, b, y) << " Радиус описанной окружности = " << RadiusOutside(a, b, c) << endl;
+    }
 }
